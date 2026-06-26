5a56dbfeefdf0940b97dc7749f17ebf55f417295
muhammadUsmanDasti <muhammadusmandasti07@gmail.com>
2026-06-26 11:43:19 +0500

    Time: 0 ms (100%), Space: 31.4 MB (7.18%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int index = -1;
+        int start = 0;
+        int end = nums.size()-1;
+        while(start<=end){
+            int mid= start + (end-start)/2;
+            if(target==nums[mid]){
+                return mid;
+            }
+            else if(target<nums[mid]){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return index;
+
+    }
+};
