e03a34d5740f3c0ccdfe5283d5c99ba7ee62ff37
muhammadUsmanDasti <muhammadusmandasti07@gmail.com>
2026-06-27 15:49:38 +0500

    Time: 0 ms (100%), Space: 13.4 MB (7.29%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
+    bool BSInARow(vector<vector<int>>& matrix, int target,int targetRow){
+        int column = matrix[0].size();
+        int low = 0;
+        int high = column-1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(target == matrix[targetRow][mid]){
+                return true;
+            }
+            else if(target < matrix[targetRow][mid]){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
+
+
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        bool flag = false;
         int row = matrix.size();
         int column = matrix[0].size();
-        
         //binary search approuch to find the row that contain the target
         int startRow = 0;
         int endRow = row - 1;
         while(startRow <= endRow){
             int midRow = startRow + (endRow - startRow) / 2;
             if(target >= matrix[midRow][0] && target <= matrix[midRow][column-1]){
-                int low = 0;
-                int high = column-1;
-                while(low <= high){
-                    int mid = low + (high - low) / 2;
-                    if(target == matrix[midRow][mid]){
-                        flag = true;
-                        break;
-                    }
-                    else if(target < matrix[midRow][mid]){
-                        high = mid - 1;
-                    }
-                    else{
-                        low = mid + 1;
-                    }
-                }
-                break;
+                return BSInARow(matrix,target,midRow);
             }
             else if(target < matrix[midRow][0]){
                 endRow = midRow - 1;
@@ -35,7 +38,7 @@ public:
                 startRow = midRow + 1;
             }
         }
-        return flag;
+        return false;
     }
     
 };
