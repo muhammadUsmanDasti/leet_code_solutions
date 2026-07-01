d90563e4200b3ec75e9ca40df1ded3fd09160f1a
muhammadUsmanDasti <muhammadusmandasti07@gmail.com>
2026-07-01 13:19:47 +0500

    Time: 3 ms (28.32%), Space: 95.2 MB (61.61%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,39 +1,43 @@
 class Solution {
 public:
-    vector<int> merge(vector<int>& nums1, vector<int>& nums2){
-        int j = 0, k = 0;
-        vector<int> nums3;
-        while(j < nums1.size() && k < nums2.size()){
-            if(nums1[j] <= nums2[k]) {
-                nums3.push_back(nums1[j]);
-                j++;
-            }
-            else if(nums1[j] >= nums2[k]) {
-                nums3.push_back(nums2[k]);
-                k++;
-            }
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2){
+        if(nums1.size() > nums2.size()){
+            return findMedianSortedArrays(nums2, nums1);
         }
-        while(j < nums1.size()){
-            nums3.push_back(nums1[j]);
-            j++;
-        }
-        while(k < nums2.size()){
-            nums3.push_back(nums2[k]);
-            k++;
-        }
-        return nums3;
-    }
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> nums3 = merge(nums1,nums2);
-        int n = nums3.size();
-        double median = 0;
-        if(nums3.size() % 2 == 0){
-            median = ( nums3[n / 2] + nums3[(n / 2) - 1] ) / 2.0;
-        }
-        else{
-            median = nums3[n / 2];
-        }
+        int m = nums1.size();
+        int n = nums2.size();
+        int total = m + n;
+        int half = total / 2;
         
-        return median;
+        int l = 0;
+        int r = m;
+
+        while(l <= r) {
+            int mid = l + (r - l) / 2;
+            int maxLeft_nums1 = (mid == 0) ? INT_MIN : nums1[mid - 1];
+            int minRight_nums1 = (mid == m) ? INT_MAX : nums1[mid];
+
+            int nums2_Left = (total + 1) / 2 - mid;
+            int maxLeft_nums2 = (nums2_Left == 0) ? INT_MIN : nums2[nums2_Left - 1];
+            int minRight_nums2 = (nums2_Left == n) ? INT_MAX : nums2[nums2_Left];
+            if(maxLeft_nums1 <= minRight_nums2 && maxLeft_nums2 <= minRight_nums1) {
+                if(total % 2 == 0){
+                    return ( max(maxLeft_nums1 , maxLeft_nums2) + min(minRight_nums1 , minRight_nums2) ) / 2.0;
+                }
+                else {
+                    return max(maxLeft_nums1, maxLeft_nums2);
+                }
+            }
+            else if(maxLeft_nums1 > minRight_nums2) {
+                 r = mid - 1; 
+            }
+            else {
+                l = mid + 1; 
+            }
+        }
+        return 0.0;
+        
+        
+        
     }
 };
