0bb40730305d92324e96b17e557d64f14e79152d
muhammadUsmanDasti <muhammadusmandasti07@gmail.com>
2026-07-03 17:40:16 +0500

    Time: 2 ms (36.28%), Space: 9.8 MB (94.35%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -4,17 +4,16 @@ public:
         if(s.size() == 0){
             return true;
         }
-        string result = "";
-        for (int k = 0; k < s.size(); k++){
-            char lowerChar = tolower(s[k]);
-            if((lowerChar >= 'a' && lowerChar <= 'z') || (lowerChar >= '0' && lowerChar <= '9')){
-                result.push_back(lowerChar);
-            }
-        }
         int i = 0;
-        int j = result.size() - 1;
-        while(i <= j){
-            if(result[i] != result[j]){
+        int j = s.size() - 1;
+        while (i < j){
+            while(i < j && !(isalnum(s[i]))){
+                i++;
+            }
+            while(i < j && !(isalnum(s[j]))){
+                j--;
+            }
+            if(tolower(s[i]) != tolower(s[j])){
                 return false;
                 break;
             }
