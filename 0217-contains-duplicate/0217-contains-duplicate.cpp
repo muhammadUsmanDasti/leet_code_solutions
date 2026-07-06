83f8679f49f9b2869b110cb8ae740331fdc4a890
muhammadUsmanDasti <muhammadusmandasti07@gmail.com>
2026-07-06 12:06:47 +0500

    Time: 0 ms (100%), Space: 90.3 MB (76.82%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int MIN = INT_MIN;
+        int hashSize = nums.size() * 2;
+        vector<int> hashTable(hashSize , MIN);
+        for(int i = 0; i < nums.size(); i++) {
+            int index = hashFunction(nums[i] , hashSize);
+            while(hashTable[index] != MIN) {
+                if(hashTable[index] == nums[i]){
+                    return true;
+                }
+                else {
+                    index = (index + 1) % hashSize;
+                }
+            }
+            hashTable[index] = nums[i];
+        }
+        return false;
+    }
+private:
+    int hashFunction(int key , int hashSize) {
+        return ((key % hashSize) + hashSize) % hashSize;
+    }
+};
