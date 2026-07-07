1cfc5ff58eeeb4e13ac6dd6b4edbc27904c0c3f8
muhammadUsmanDasti <muhammadusmandasti07@gmail.com>
2026-07-07 15:05:20 +0500

    Time: 0 ms (100%), Space: 15.3 MB (8.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,34 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        int MIN = INT_MIN;
+        int hashSize = nums.size() * 2;
+        vector<pair<int,int>> hashTable(hashSize , {MIN , -1});
+        for (int i = 0; i < nums.size(); i++) {
+            int index = hashFunction(nums[i] , hashSize);
+            while(hashTable[index] != make_pair(MIN , -1)) {
+                index = (index + 1) % hashSize;
+            }
+            hashTable[index] = {nums[i] , i};
+        }
 
-    for(int x=0;x<nums.size();x++){
-        for(int y=x+1;y<nums.size();y++){
-            if(nums[x]+nums[y]==target){
-                
-                return {x,y};
+        for(int j = 0; j < nums.size(); j++) {
+            int x = nums[j];
+            int y = target - x;
+            int indexY = hashFunction(y , hashSize);
+            while(hashTable[indexY] != make_pair(MIN,-1)) {
+                if(hashTable[indexY].first == y && hashTable[indexY].second != j) {
+                    return {j , hashTable[indexY].second};
+                }
+                else {
+                    indexY = (indexY + 1) % hashSize;
+                }
             }
         }
+        return {};
     }
-     return {};
+private:
+    int hashFunction(int key , int hashSize) {
+        return ((key % hashSize) + hashSize) % hashSize;
     }
 };
