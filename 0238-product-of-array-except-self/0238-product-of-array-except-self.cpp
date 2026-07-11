10aa62492d4de38eda125e4c8ed23c22f951d991
muhammadUsmanDasti <muhammadusmandasti07@gmail.com>
2026-07-11 13:00:30 +0500

    Time: 0 ms (100%), Space: 40.4 MB (45.31%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -3,16 +3,14 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
         vector<int> result(n,1);
-        int last_suffix = 1;
-        int final_suffix = 1;
+        int suffix = 1;
         for(int i = 1; i < n; i++) {
             result[i] = result[i-1] * nums[i-1];
         }
         for(int i = n-2; i >= 0; i--) {
 
-            final_suffix = (last_suffix * nums[i+1]);
-            result[i] *= final_suffix;
-            last_suffix = final_suffix;
+            suffix *= nums[i+1];
+            result[i] *= suffix;
         }
         return result;
     }
