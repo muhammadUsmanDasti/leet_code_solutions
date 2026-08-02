c502a92c0fed246e5cd247554035765f72a4b486
Muhammad Usman Dasti <muhammadusmandasti07@gmail.com>
2026-08-02 13:01:53 +0500

    Time: 4 ms (14.26%), Space: 9.8 MB (94.54%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        if(s.size() == 0){
-            return true;
-        }
         int i = 0;
         int j = s.size() - 1;
-        while (i < j){
-            while(i < j && !(isalnum(s[i]))){
+        while(i < j) {
+            while(i < j && !(isalnum(s[i]))) {
                 i++;
             }
-            while(i < j && !(isalnum(s[j]))){
+            while(i < j && !(isalnum(s[j]))) {
                 j--;
             }
             if(tolower(s[i]) != tolower(s[j])){
                 return false;
-                break;
             }
             i++;
             j--;
