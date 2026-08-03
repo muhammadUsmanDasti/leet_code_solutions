6aeb001843f31a4451ea8f45f37a3be9bef02217
Muhammad Usman Dasti <muhammadusmandasti07@gmail.com>
2026-08-03 14:51:12 +0500

    Time: 53 ms (31.99%), Space: 31 MB (24.47%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size(); i++) {
+            if(i > 0) {
+                if(nums[i] == nums[i - 1]){
+                    continue;
+                }
+            }
+            int j = i + 1;
+            int k = nums.size() - 1;
+            while(j < k) {
+                long sum = nums[i] + nums[j] + nums[k];
+                if(sum == 0) {
+                    vector<int> triplet = {nums[i], nums[j], nums[k]};
+                    result.push_back(triplet);
+                    while(j < k && nums[j] == nums[j + 1]) {
+                        j++;
+                        continue;
+                    }
+                    while(j < k && nums[k] == nums[k - 1]) {
+                        k--;
+                        continue;
+                    }
+                    j++;
+                    k--;
+                }
+                else if(sum > 0) {
+                    k--;
+                }
+                else {
+                    j++;
+                }
+
+            }
+            
+        }
+        return result;
+    }
+};
