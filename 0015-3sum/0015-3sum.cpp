d258776246dfc0b174cd66eed7083ffe88eed35d
Muhammad Usman Dasti <muhammadusmandasti07@gmail.com>
2026-08-03 15:53:13 +0500

    Time: 63 ms (16.85%), Space: 31.2 MB (16.85%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -17,13 +17,13 @@ public:
                     vector<int> triplet = {nums[i], nums[j], nums[k]};
                     result.push_back(triplet);
                     j++;
-                    k--;
+                    // k--;
                     while(j < k && nums[j] == nums[j - 1]) {
                         j++;
                     }
-                    while(j < k && nums[k] == nums[k + 1]) {
-                        k--;
-                    }
+                    // while(j < k && nums[k] == nums[k + 1]) {
+                    //     k--;
+                    // }
                 }
                 else if(sum > 0) {
                     k--;
