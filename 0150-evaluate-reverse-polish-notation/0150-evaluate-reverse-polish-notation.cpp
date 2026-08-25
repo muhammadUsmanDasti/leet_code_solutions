2102e699fdf783c2a67c9f66c49ac4b4de8dbee6
Muhammad Usman Dasti <muhammadusmandasti07@gmail.com>
2026-08-25 13:21:19 +0500

    Time: 0 ms (100%), Space: 17.5 MB (8.14%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        vector<string> stack;
+        for (int i = 0; i < tokens.size(); i++) {
+            if (tokens[i] != "+" && tokens[i] != "-" && tokens[i] != "*" && tokens[i] != "/") {
+                stack.push_back(tokens[i]);
+            }
+            else if (tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/" || tokens[i] == "/" && stack.size() >= 2) {
+                int result = 0;
+                int second = stoi(stack.back());
+                stack.pop_back();
+                int first = stoi(stack.back());
+                stack.pop_back();
+                if(tokens[i] == "+") {
+                    result = first + second;
+                }
+                else if(tokens[i] == "-") {
+                    result = first - second;
+                }
+                else if(tokens[i] == "*") {
+                    result = first * second;
+                }
+                else if(tokens[i] == "/") {
+                    result = first / second;
+                }
+                stack.push_back(to_string(result));
+            }
+
+           
+            
+        }
+        return stoi(stack.back());
+    }
+};
