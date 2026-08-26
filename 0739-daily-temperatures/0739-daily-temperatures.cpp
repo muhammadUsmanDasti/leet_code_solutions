47130d56cedbdeca6ea25667af773e3262b9ef23
Muhammad Usman Dasti <muhammadusmandasti07@gmail.com>
2026-08-26 13:18:36 +0500

    Time: 13 ms (89.72%), Space: 107.8 MB (12.26%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> answer(temperatures.size(),0);
+        vector<pair<int,int>> stack;
+        for (int i = 0; i < temperatures.size(); i++) {
+            if (stack.empty()) {
+                stack.push_back({temperatures[i], i});
+            } else if(stack.back().first > temperatures[i]) {
+                stack.push_back({temperatures[i], i});
+            }
+            while (!(stack.empty()) && stack.back().first < temperatures[i]) {
+                answer[stack.back().second] = i - stack.back().second;
+                stack.pop_back();
+            }
+            stack.push_back({temperatures[i], i});
+        }
+        return answer;
+    }
+};
