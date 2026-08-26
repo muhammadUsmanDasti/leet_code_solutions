6898db2eb3b2bbeebda2b2f58c23630bf8e0480a
Muhammad Usman Dasti <muhammadusmandasti07@gmail.com>
2026-08-26 13:24:56 +0500

    Time: 17 ms (76.46%), Space: 107.8 MB (12.39%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -6,14 +6,19 @@ public:
         for (int i = 0; i < temperatures.size(); i++) {
             if (stack.empty()) {
                 stack.push_back({temperatures[i], i});
-            } else if(stack.back().first > temperatures[i]) {
+            } else if(stack.back().first >= temperatures[i]) {
                 stack.push_back({temperatures[i], i});
             }
+            bool flag = false;
             while (!(stack.empty()) && stack.back().first < temperatures[i]) {
                 answer[stack.back().second] = i - stack.back().second;
                 stack.pop_back();
+                flag = true;
             }
-            stack.push_back({temperatures[i], i});
+            if(flag) {
+                stack.push_back({temperatures[i], i});
+            }
+            
         }
         return answer;
     }
