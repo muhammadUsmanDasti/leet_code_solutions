d76001297f33b16eb70acb7fa198844ac97fa9b9
Muhammad Usman Dasti <muhammadusmandasti07@gmail.com>
2026-08-27 15:22:55 +0500

    Time: 36 ms (80.65%), Space: 106.2 MB (51.62%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        vector<pair<int, int>> arr;
-        vector<pair<int, int>> stack;
+        vector<pair<int, int>> cars;
+        vector<double> stack;
         for (int i = 0; i < position.size(); i++) {
-            arr.push_back({position[i], speed[i]});
+            cars.push_back({position[i], speed[i]});
         }
-        sort(arr.begin(), arr.end(), [](const auto &a, const auto &b) {
+        sort(cars.begin(), cars.end(), [](const auto &a, const auto &b) {
             return a.first > b.first;
         });
-        int noOfFleets = 0;
-        for(int i = 0; i < arr.size(); i++) {
-            if(!stack.empty() && stack.back().second < arr[i].second){
-                float first = static_cast<float>(static_cast<float>(target - stack.back().first)) / stack.back().second;
-                float second = static_cast<float>(static_cast<float>(target - arr[i].first)) / arr[i].second;
-                if (second > first) {
-                    stack.push_back(arr[i]);
-                    noOfFleets++;
-                }
-            }
-            else {
-                stack.push_back(arr[i]);
-                noOfFleets++;
+        for(int i = 0; i < cars.size(); i++) {
+            double time = (double)(target - cars[i].first) / cars[i].second;
+            if(stack.empty() || time > stack.back()){
+
+                stack.push_back(time);
             }
         }
-        return noOfFleets;
+        return stack.size();
     }
 };
